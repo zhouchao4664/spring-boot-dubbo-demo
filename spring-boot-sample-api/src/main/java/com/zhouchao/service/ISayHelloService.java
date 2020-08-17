d800ae184f58bb8c17aad38c2c0e8e28c54031b7
zhouchao <chao.zhou@raykite.com>
2020-08-17 22:49:19 +0800

    增加rest接口

--- spring-boot-sample-api/src/main/java/com/zhouchao/service/ISayHelloService.java
+++ spring-boot-sample-api/src/main/java/com/zhouchao/service/ISayHelloService.java
@@ -1,9 +1,17 @@
 package com.zhouchao.service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 /**
  * 2020/8/9
  * zhouchao
  */
+
+@Path("/")
 public interface ISayHelloService {
+
+    @GET
+    @Path("/say")
     String sayHello(String msg);
 }
